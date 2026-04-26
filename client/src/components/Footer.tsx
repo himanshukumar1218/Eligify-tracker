94967a027b6ad11f6a5f9fbe61baeeedb3dc92c6
Himanshu Kumar <himanshu99071@gmail.com>
2026-04-27 03:20:38 +0530

    refactor: update social links and clean up footer/contact UI

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -18,12 +18,8 @@ const Twitter = (props: any) => (
   <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" /></svg>
 );
 
-const Linkedin = (props: any) => (
-  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect width="4" height="12" x="2" y="9" /><circle cx="4" cy="4" r="2" /></svg>
-);
-
-const Youtube = (props: any) => (
-  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 2-2 69.44 69.44 0 0 1 15 0 2 2 0 0 1 2 2 24.12 24.12 0 0 1 0 10 2 2 0 0 1-2 2 69.44 69.44 0 0 1-15 0 2 2 0 0 1-2-2z" /><path d="m10 15 5-3-5-3z" /></svg>
+const Instagram = (props: any) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
 );
 
 const Footer: React.FC = () => {
@@ -45,11 +41,9 @@ const Footer: React.FC = () => {
   };
 
   const socials = [
-    { name: 'Twitter', icon: Twitter, href: '#', color: 'hover:text-[#1DA1F2]' },
-    { name: 'LinkedIn', icon: Linkedin, href: '#', color: 'hover:text-[#0A66C2]' },
-    { name: 'YouTube', icon: Youtube, href: '#', color: 'hover:text-[#FF0000]' },
-    { name: 'Telegram', icon: Send, href: '#', color: 'hover:text-[#229ED9]' },
-    { name: 'GitHub', icon: Github, href: '#', color: 'hover:text-[#f0f6fc]' },
+    { name: 'X', icon: Twitter, href: 'https://x.com/geteligify', color: 'hover:text-[#1DA1F2]' },
+    { name: 'Instagram', icon: Instagram, href: 'https://www.instagram.com/geteligify/', color: 'hover:text-[#E4405F]' },
+    { name: 'GitHub', icon: Github, href: 'https://github.com/himanshukumar1218', color: 'hover:text-[#f0f6fc]' },
   ];
 
   return (
